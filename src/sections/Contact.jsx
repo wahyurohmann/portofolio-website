0ad80306827b874776cf3535eab01d33de8ef420
wahyurohmann <wahyurohman249@gmail.com>
2026-04-27 14:00:34 +0700

    edit text

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -157,7 +157,7 @@ export default function Contact() {
             </motion.h3>
 
             <motion.p className="contact__description" variants={fadeUp}>
-              Whether you have a project idea, an internship opportunity, or just want to
+              Whether you have a project idea, an internship opportunity or just want to
               say hello, my inbox is always open. I'll do my best to get back
               to you within 24 hours.
             </motion.p>
